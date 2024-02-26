dd578c76494fd0bf18b8078992d7551431cf26e0
NaraeJang <j.naaaarae@gmail.com>
2024-02-26 18:54:32 -0500

    Complete wedding party

--- src/componenets/WeddingParty.jsx
+++ src/componenets/WeddingParty.jsx
@@ -1,4 +1,78 @@
+import { TitleNoDivider } from '../subCompoenets';
+import { weddingPartyImage } from '../context/weddingPartyImages';
+import { useWegicContext } from '../App';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 const WeddingParty = () => {
-  return <div>WeddingParty</div>;
+  const { women, men } = weddingPartyImage;
+  const { isFrench } = useWegicContext();
+
+  return (
+    <section id="wedding-party" className="wedding-party container">
+      <TitleNoDivider
+        titleEn="WEDDING PARTY"
+        titleFr="LES MEMBRES DU MARRIAGE"
+      />
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper3">
+        <SwiperSlide>
+          <div className="wedding-party__profiles">
+            {women.map((person, index) => {
+              const { path, name, titleEn, titleFr } = person;
+
+              return (
+                <div
+                  className="wedding-party__profiles--detail"
+                  key={index + name}>
+                  <img src={path} alt={`${name} photo`} />
+                  <h4>{name}</h4>
+                  <h5
+                    dangerouslySetInnerHTML={{
+                      __html: isFrench ? titleFr : titleEn,
+                    }}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="wedding-party__profiles">
+            {men.map((person, index) => {
+              const { path, name, titleEn, titleFr } = person;
+
+              return (
+                <div
+                  className="wedding-party__profiles--detail"
+                  key={index + name}>
+                  <img src={path} alt={`${name} photo`} />
+                  <h4>{name}</h4>
+                  <h5
+                    dangerouslySetInnerHTML={{
+                      __html: isFrench ? titleFr : titleEn,
+                    }}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
 };
 export default WeddingParty;
---
+++ src/context/weddingPartyImages.js
@@ -0,0 +1,50 @@
+import party1 from '../assets/images/wedding-party/photo-chris.png';
+import party2 from '../assets/images/wedding-party/Photo-Emily.png';
+import party3 from '../assets/images/wedding-party/photo-igor.png';
+import party4 from '../assets/images/wedding-party/photo-wily.png';
+import party5 from '../assets/images/wedding-party/Photo-Laeticia.png';
+import party6 from '../assets/images/wedding-party/Photo-Meredyth.png';
+import party7 from '../assets/images/wedding-party/photo-tudor.png';
+import party8 from '../assets/images/wedding-party/Photo-Veronique.png';
+
+export const weddingPartyImage = {
+  women: [
+    {
+      path: party6,
+      name: 'Meredyth',
+      titleEn: 'Maid of Honor',
+      titleFr: `Demoiselle d&#8217;honneur`,
+    },
+    {
+      path: party5,
+      name: 'Laeticia',
+      titleEn: 'Bridesmaid',
+      titleFr: 'Bridesmaid',
+    },
+    {
+      path: party8,
+      name: 'Veronique',
+      titleEn: 'Bridesmaid',
+      titleFr: 'Bridesmaid',
+    },
+    {
+      path: party2,
+      name: 'Emily',
+      titleEn: 'Bridesmaid',
+      titleFr: 'Bridesmaid',
+    },
+  ],
+  men: [
+    {
+      path: party1,
+      name: 'Chris',
+      titleEn: 'Best Man',
+      titleFr: `Garçon d&#8217;honneur`,
+    },
+
+    { path: party3, name: 'Igor', titleEn: 'Groomsman', titleFr: 'Groomsman' },
+    { path: party4, name: 'Iman', titleEn: 'Groomsman', titleFr: 'Groomsman' },
+
+    { path: party7, name: 'Tudor', titleEn: 'Groomsman', titleFr: 'Groomsman' },
+  ],
+};

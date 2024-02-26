1e70fbfb19c1e87eab7dd3ebee90f781236539ef
NaraeJang <j.naaaarae@gmail.com>
2024-02-26 14:03:44 -0500

    Complete Contact

--- src/componenets/Contact.jsx
+++ src/componenets/Contact.jsx
@@ -10,6 +10,24 @@ const Contact = () => {
         titleEn="CONTACT GROOM & BRIDE"
         titleFr="CONTACTER LES MARIÉS"
       />
+      <div class="contact__content">
+        <h4>{isFrench ? 'contacter' : 'contact'} Iaroslav</h4>
+        <a href="tel:+11234567890">
+          <i class="fa-solid fa-phone"></i>
+        </a>
+        <a href="mailto:peter@wegic.com">
+          <i class="fa-solid fa-envelope"></i>
+        </a>
+      </div>
+      <div class="contact__content">
+        <h4>{isFrench ? 'contacter' : 'contact'} Sumin</h4>
+        <a href="tel:+11234567890">
+          <i class="fa-solid fa-phone"></i>
+        </a>
+        <a href="mailto:Sarah@wegic.com">
+          <i class="fa-solid fa-envelope"></i>
+        </a>
+      </div>
     </section>
   );
 };
